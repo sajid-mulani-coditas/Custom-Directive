c6d4e833257d32ed28549e23690a93d4a03474ab
unknown <sajidmulani2712@gmail.com>
2021-08-24 10:46:39 +0530

    first commit

--- src/app/custom.directive.ts
+++ src/app/custom.directive.ts
@@ -6,8 +6,11 @@ import { Directive,ElementRef } from '@angular/core';
 export class CustomDirective {
 
   // made an instance of ElementRef
+  // ElementRef is used to access DOM element and manipulate it.
   constructor(private el: ElementRef) {
     el.nativeElement.style.backgroundColor="yellow"
+    el.nativeElement.style.fontFamily="arial"
+    el.nativeElement.style.color="purple"
    }
 
 }
